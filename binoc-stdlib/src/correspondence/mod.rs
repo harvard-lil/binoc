168e5b6909b53b1e25328593865b7a32b3a03319
Jack Cushman <jcushman@gmail.com>
2026-06-29 16:32:13 -0400

    Implement binary CDC byte-range writer

--- binoc-stdlib/src/correspondence/mod.rs
+++ binoc-stdlib/src/correspondence/mod.rs
@@ -105,6 +105,7 @@ pub fn engine_config_with_options(options: CorrespondenceOptions) -> Corresponde
             Arc::new(writers::StructuredDocumentWriter),
             Arc::new(writers::TextWriter),
             Arc::new(writers::ContainerWriter),
+            Arc::new(writers::BinaryChunkWriter),
             Arc::new(writers::FallbackWriter),
         ],
         compaction: vec![
